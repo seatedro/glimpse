8fc47770f17f90e5f5aaa3576eca3ce0d17896ef
ro <54208873+rohitp934@users.noreply.github.com>
2024-12-30 01:24:14 -0500

    Nix flake

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "blazingly fast tool for peeking at codebases";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "glimpse";
+          version = "0.4.0";
+          
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+
+          buildInputs = with pkgs; [ ];
+
+          meta = with pkgs.lib; {
+            description = "A blazingly fast tool for peeking at codebases";
+            homepage = "https://github.com/seatedro/glimpse";
+            license = licenses.mit;
+            maintainers = ["seatedro"];
+            platforms = platforms.all;
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rust-bin.stable.latest.default
+            pkg-config
+          ];
+        };
+      }
+    );
+}
