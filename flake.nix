516e5e103a7f62bd6cf819de628ba7ad4844efc4
ro <54208873+rohitp934@users.noreply.github.com>
2024-12-30 17:25:05 -0500

    Bump version

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "glimpse";
-          version = "0.6.1";
+          version = "0.6.2";
           
           src = ./.;
 
