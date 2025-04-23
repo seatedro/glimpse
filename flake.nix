7d33c40d541fe8345290e2183c9cf062a7411f31
ro <git@seated.ro>
2025-04-22 20:21:19 -0700

    update version

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "glimpse";
-          version = "0.7.1";
+          version = "0.7.2";
 
           src = ./.;
 
