06f2bf1593419345583a0d8575fc5a700dcbc191
ro <rohit@exa.ai>
2025-07-02 17:17:47 -0700

    bump: v0.7.8

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "glimpse";
-          version = "0.7.7";
+          version = "0.7.8";
 
           src = ./.;
 
